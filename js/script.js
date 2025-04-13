8b6599007c8a55876509fa98ac9826f331f18021
Dawit <tesfayedawit22090582@gmail.com>
2025-04-13 14:04:15 +0300

    footer added

--- js/script.js
+++ js/script.js
@@ -11,3 +11,37 @@ $(document).ready(function () {
     }
   });
 });
+
+$(document).ready(function () {
+  // Footer toggle function with arrows
+  $("footer h3").on("click", function () {
+    if ($(window).width() < 768) {
+      const $h3 = $(this);
+      const $ul = $h3.next("ul");
+
+      // Toggle active class for arrow rotation
+      $h3.toggleClass("active");
+
+      // Close other open sections
+      $("footer h3").not($h3).removeClass("active").next("ul").slideUp();
+
+      // Toggle current section
+      $ul.slideToggle();
+    }
+  });
+
+  // Close all footer sections on load for mobile
+  if ($(window).width() < 768) {
+    $("footer ul").hide();
+  }
+
+  // Handle window resize
+  $(window).resize(function () {
+    if ($(window).width() >= 768) {
+      $("footer ul").show();
+      $("footer h3").removeClass("active");
+    } else {
+      $("footer ul").hide();
+    }
+  });
+});
